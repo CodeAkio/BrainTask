1bb528ce5f6f0c63538dafbac6e4e62018bdaa83
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-22 23:42:20 -0300

    Created card tasks layout and navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import { MdDone } from 'react-icons/md';
+import GlobalStyle from './styles/global';
+
+import {
+  NavBar,
+  Main,
+  Content,
+  Card,
+  Tasks,
+  Task,
+  CheckBox,
+  Label,
+} from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Brain Task</p>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <NavBar>
+        <nav>
+          <span>Brain Task</span>
+        </nav>
+      </NavBar>
+      <Main>
+        <Content>
+          <Card>
+            <Tasks>
+              <Task>
+                <CheckBox /> <Label>Lavar o carro</Label>
+              </Task>
+              <Task>
+                <CheckBox checked>
+                  <MdDone color="#fff" size={10} />
+                </CheckBox>
+                <Label>Devolver o livro na biblioteca</Label>
+              </Task>
+              <Task>
+                <CheckBox />
+                <Label>Pagar o boleto da fatura do cartão de crédito</Label>
+              </Task>
+              <Task>
+                <CheckBox /> <Label>Ir para a academia</Label>
+              </Task>
+            </Tasks>
+          </Card>
+        </Content>
+      </Main>
+    </>
   );
 }
 
---
+++ web/src/styles/index.js
@@ -0,0 +1,66 @@
+import Styled from 'styled-components';
+
+export const NavBar = Styled.div`
+  height: 80px;
+  width: 100%;
+  padding: 24px 32px;
+  background: rgba(255, 255, 255, 0.6);
+  align-items: center;
+
+  nav {
+    display: flex;
+
+    span {
+      color: #FF6184;
+      font-weight: bold;
+      font-size: 24px;
+    }
+  }
+`;
+
+export const Main = Styled.main`
+  display: flex;
+  justify-content: center;
+  margin-top: 64px;
+`;
+
+export const Content = Styled.div`
+  width: 750px;
+`;
+
+export const Card = Styled.div`
+  background: #fff;
+  border-radius: 6px;
+  padding: 16px 24px;
+`;
+
+export const Tasks = Styled.ul``;
+
+export const Task = Styled.li`
+  display: flex;
+  align-items: center;
+  margin: 8px 0;
+`;
+
+export const CheckBox = Styled.span`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  border: 1px solid ${(props) => (props.checked ? '#FF6184' : '#8892B3')};
+  border-radius: 4px;
+  position: relative;
+  background: ${(props) => (props.checked ? '#FF6184' : '#fff')};
+
+  svg {
+    position: absolute;
+    left: 15%;
+    top: 50%;
+    transform: translateX(-50%);
+    transform: translateY(-50%);
+  }
+`;
+
+export const Label = Styled.span`
+  color: #8892B3;
+  margin-left: 8px;
+`;
